d2d1f4ef426bbf99f161906b38ab9c6f2aac32f3
Mohn <93068579+mohndng@users.noreply.github.com>
2022-12-20 23:49:28 +0800

    Add files via upload

---
+++ ismis/ismis.js
@@ -0,0 +1,14 @@
+function submit() {
+  setTimeout(function () {
+    alert(
+      "Login Failure"
+    );
+  }, 800);
+}
+function forgot() {
+  setTimeout(function () {
+    alert(
+      "Ask Raymund Torneado for assistance. Thank you."
+    );
+  }, 800);
+}
